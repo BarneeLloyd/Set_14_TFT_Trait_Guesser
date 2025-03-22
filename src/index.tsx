403851e1c95d2399919193ca4ce2c3542e09ba01
BarneeLloyd <barneelloyd@gmail.com>
2025-03-22 00:11:21 +0000

    added null check

--- src/index.tsx
+++ src/index.tsx
@@ -4,9 +4,16 @@ import "./styles.css";
 
 import App from "./App";
 
-const root = createRoot(document.getElementById("root"));
-root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+// Get the root element
+const rootElement = document.getElementById("root");
+
+if (rootElement) {
+  const root = createRoot(rootElement);
+  root.render(
+    <StrictMode>
+      <App />
+    </StrictMode>
+  );
+} else {
+  console.error('Root element not found');
+}
